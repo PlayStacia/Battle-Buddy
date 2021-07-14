06ef3ee353f2647dfd3b3ad98d0fa1cd1e7d6666
Stacia <staciaAnn.aguilar@gmail.com>
2021-07-14 16:59:47 -0700

    test push bg color

--- BattleBuddy/battlebuddy/battlebuddy/settings.py
+++ BattleBuddy/battlebuddy/battlebuddy/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
-from pathlib import Path
+# import mimetypes
+# mimetypes.add_type("text/css", ".css", True)
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'STATIC')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
